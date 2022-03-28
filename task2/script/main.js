fcbd22cb6dcfd745f0eadca642321a59466cdd1f
1Donat1 <79876158+1Donat1@users.noreply.github.com>
2022-03-29 00:50:57 +0300

    Create main.js

---
+++ task2/script/main.js
@@ -0,0 +1,123 @@
+'use strict'
+let num1=+prompt('Number 1')
+//1
+// let num2=num1===0 ? 'Верно':'Неверно';
+// alert(num2);
+
+ //2
+// let num2=num1>0 ? 'Верно':'Неверно';
+// alert(num2);
+
+//3
+// let num2=num1<0 ? 'Верно':'Неверно';
+// alert(num2);
+
+//4
+// if(num1===0 || num1>0){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+
+//5
+// if(num1===0 || num1<0){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+
+ // 6
+// if(num1!=0 ){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+
+// 7
+// if(num1='test' ){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+
+// 8
+// if(num1===1){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+//9
+// if(num1>0 && num1<5){
+//     alert('Верно');
+
+// }
+// else{
+//     alert('Неверно');
+// }
+//10
+// if(num1===0 || num1===2){
+//     alert(num1 + 7);
+
+// }
+// else{
+//     alert(num1 / 10);
+// }
+//11
+// let num2=+prompt('Number 2')
+// if(num1<=1 && num2>=3){
+//     alert(num1 + num2);
+
+// }
+// else{
+//     alert(num1 - num2);
+// }
+//12
+// let num2=+prompt('Number 2')
+// if(num1>2 && num1<11||num2>=6 && num2<14){
+//     alert('Верно');
+// }
+
+// else{
+//     alert('неверно');
+// }
+//13
+switch(num1){
+case 1:
+    alert('Winter');
+    break;
+case 2:
+    alert('Spring');
+    break;
+case 3:
+    alert('Summer');
+    break;
+    case 4:
+        alert('Autumn')
+        break;
+        default:
+            alert('Type number of the season' )
+            break;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
