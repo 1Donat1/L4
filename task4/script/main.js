27376851314b47a82e3142756425e278b2771e0a
1Donat1 <79876158+1Donat1@users.noreply.github.com>
2022-04-01 16:12:37 +0300

    Create main.js

---
+++ task4/script/main.js
@@ -0,0 +1,66 @@
+'use strict'
+// //1
+// let x = 10;
+// let y = 7;
+// if(x>y){
+//  alert('x больше чем у');
+// }
+// else{
+//     alert('x меньше чем у');
+// };
+
+// //2
+// let num1=+prompt('Введите число ')
+// if(num1 % 2){
+//     alert(`число ${num1} нечетное `);
+// }
+// else{
+//     alert(`число ${num1} четное `);
+// };
+
+// //3
+// let number = +prompt("Введите число: ");
+// let number2='';
+// let number3=((number + "").length);
+
+//     if (number===null||number=='' ) {
+//       number2=("Вы нажали \"Отмена\"");
+//       number3=0;
+//     } else if (number <0 ) {
+//       number2=("Вы ввели отрицательное число");
+//     } else if (number === false) {
+//       number2=("Вы ввели ноль");
+//     } else if (isNaN(number)) {
+//       number2=("Вы ввели не число");
+//     } else {
+//       number2=("Вы вввели положительное число");
+//     };
+// alert(`${number2}\n В этом числе ${number3} цифр`);
+
+// //4
+// let num1=+prompt('Fisrt number');
+// let num2=+prompt('Second number');
+// let num3=+prompt('Third number');
+// if(num1>num2 && num1>num3){
+//   alert('First number is the biggest' );
+// }
+// else if (num2>num1 && num2>num3){
+//   alert('Second number is the biggest' );
+// }
+// else if (num3>num2 && num3>num2){
+//   alert('Third number is the biggest' );
+// }
+// else{
+//   alert('Numbers are equal');
+// };
+// //5
+// let num1=+prompt('Fisrt side');
+// let num2=+prompt('Second side');
+// let num3=+prompt('Third side');
+// if(num1 + num2<num3 || num2 + num3<num1 ||num1 + num3<num2 ){
+//   alert('A triangle cannot exist with such lengts of sides');
+// }
+// else{
+//   alert('Good');
+// };
+
