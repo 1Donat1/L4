edee8940003ae4b2c232791b3b93358dd819c995
1Donat1 <79876158+1Donat1@users.noreply.github.com>
2022-04-01 16:23:54 +0300

    Update main.js

--- task1/js/main.js
+++ task1/js/main.js
@@ -1,19 +1,22 @@
 'use strict'
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-switch(numOrStr){
-    case'null':
-    console.log('вы отменили');
-    break;
-    case '':
-    console.log('Empty String');
-    break;
-    case !isNaN(+numOrStr)||numOrStr:
-    console.log(' number is Ba_NaN');
-    break;
-    default:
-        console.log('OK!')
-
-    
-}
-
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+// switch(numOrStr){
+//     case'null':
+//     console.log('вы отменили');
+//     break;
+//     case '':
+//     console.log('Empty String');
+//     break;
+//     case !isNaN(+numOrStr)||numOrStr:
+//     console.log(' number is Ba_NaN');
+//     break;
+//     default:
+//         console.log('OK!')
+// }
+// if(numOrStr === null || numOrStr.trim() === ''|| isNaN( +numOrStr ) ) {
+//     console.log(("Вы нажали \"Отмена\" or number is Ba_NaN or Empty String"))
+// } 
+//  else {
+//     console.log('OK!')
+// }
